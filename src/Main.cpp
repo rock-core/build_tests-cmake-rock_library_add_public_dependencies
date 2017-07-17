dfb570d5b7e442bcee3a51acee6957671abff09a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-07-17 16:44:34 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <rock_library_add_public_dependencies/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    rock_library_add_public_dependencies::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
