2e46ec0c9347e5c52ffe1bda8e11cb4a0a8a0172
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-07-17 18:36:14 -0300

    Initial commit

--- src/Dummy.cpp
+++ src/Dummy.cpp
@@ -1,4 +1,6 @@
 #include "Dummy.hpp"
+#include <plain_package/Dummy.hpp>
+#include <another_plain_package/Dummy.hpp>
 #include <iostream>
 
 using namespace std;
@@ -6,5 +8,10 @@ using namespace rock_library_add_public_dependencies;
 
 void DummyClass::welcome()
 {
+    plain_package::DummyClass plain;
+    plain.welcome();
+    another_plain_package::DummyClass another;
+    another.welcome();
+
     cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
 }
